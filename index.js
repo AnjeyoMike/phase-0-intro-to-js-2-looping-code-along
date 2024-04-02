7422526e49c9284a3f00ff20355c2a3034f2ed89
Anjeyo Mike <mike.matekeya@student.moringaschool.com>
2024-04-02 14:44:33 +0300

    loopin code along done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+//Write yore solution in this file 
+function writeCards(names, happy = "happy"){
+    let card =[];
+    for (let r = 0; r < names.length; r++){
+        card.push(`Thank you, ${names[r]}, for the wonderful ${happy} gift!`);
+    }
+
+return card;
+}
+function countDown(number){
+
+
+    for (let index  = number; index >=0 ; index--) {
+        console.log(index)
+        
+    }
+}
